19cad6fd15a97d588ba204a077e8aded90ca79ad
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-14 23:13:50 +0530

    ADDING : Switch case method

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,12 +4,12 @@ package com.bridgelabz;
  * Purpose - Employee Wage Computation program
  *
  * @author - Aviral N
- * @version - 1.4
+ * @version - 1.5
  * date     - 14-08-2021
  */
 public class EmpWage {
 
-    public static final int IS_PRESENT = 1;
+    public static final int IS_PRESENT = 1, IS_PART_TIME_PRESENT = 2;
     public static final int FULL_TIME_WORKING_HR = 8, PART_TIME_WORKING_HR = 4;
 
     /**
@@ -33,17 +33,13 @@ public class EmpWage {
 
     /**
      * Purpose - Calculates daily wage if emp is  present
-     *
-     * @return daily wage of EMP
      */
-    public static int dailyWage(int workingHrPerDay) {
+    public static void dailyWage(int workingHrPerDay) {
 
         int WAGE_PER_HRS = 20;
 
         int dailyWage = WAGE_PER_HRS * workingHrPerDay;
         System.out.println("Daily wage: " + dailyWage);
-
-        return dailyWage;
     }
 
     /**
@@ -73,12 +69,36 @@ public class EmpWage {
         return workingHrPerDay;
     }
 
+    public static int switchCase() {
+
+        int workingHrPerDay;
+        int rand = (int) (Math.random() * 3);
+
+        switch (rand) {
+
+            case IS_PRESENT:
+                System.out.println("Employee is full time Present");
+                workingHrPerDay = FULL_TIME_WORKING_HR;
+                break;
+
+            case IS_PART_TIME_PRESENT:
+                System.out.println("Employee is part time Present");
+                workingHrPerDay = PART_TIME_WORKING_HR;
+                break;
+
+            default:
+                System.out.println("Employee is absent");
+                workingHrPerDay = 0;
+        }
+        return workingHrPerDay;
+    }
+
     /**
      * Purpose - Execution of all methods
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        dailyWage(partTimeFullTime());
+        dailyWage(switchCase());
     }
 }
