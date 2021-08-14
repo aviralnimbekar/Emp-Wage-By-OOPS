92bde96faf84a7322c2451b15ae2abc140bb4d96
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-14 18:47:11 +0530

    ADDING : Discription of class and objects

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,7 +1,17 @@
 package com.bridgelabz;
 
+/**
+ * Purpose - Employee Wage Computation program
+ *
+ * @author - Aviral N
+ * @version - 1.0
+ * date     - 14-08-2021
+ */
 public class EmpWage {
 
+    /**
+     * Purpose - Execution of all objects
+     */
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
     }
