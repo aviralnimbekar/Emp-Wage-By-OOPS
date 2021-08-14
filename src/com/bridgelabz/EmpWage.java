da563f227f0dd92ddeea54b24a72ea86d7df4e41
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-14 19:30:32 +0530

    ADDING : Daily wage of EMP computation program

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,23 +4,45 @@ package com.bridgelabz;
  * Purpose - Employee Wage Computation program
  *
  * @author - Aviral N
- * @version - 1.1
+ * @version - 1.2
  * date     - 14-08-2021
  */
 public class EmpWage {
 
-    /**
-     * Purpose - checks whether a EMP is present or absent
-     */
-    public static void attendanceCheck() {
+    public static final int IS_PRESENT = 1, WAGE_PER_HRS = 20;
 
-        int IS_PRESENT = 1;
+    /**
+     * Purpose - checks whether an EMP is present or absent
+     *
+     * @return - random number generated (empCheck)
+     */
+    public static int attendanceCheck() {
+
         int empCheck = (int) (Math.random() * 2);
 
         if (empCheck == IS_PRESENT)
             System.out.println("Employee is Present");
         else
             System.out.println("Employee is Absent");
+
+        return empCheck;    //random number generated will be returned
+    }
+
+    /**
+     * Purpose - Calculates daily wage if emp is  present
+     *
+     * @return daily wage of EMP
+     */
+    public static int dailyWage() {
+
+        int empCheck = attendanceCheck();
+        int empWorkingHrs = 0;
+
+        if (empCheck == IS_PRESENT)
+            empWorkingHrs = 8;
+
+        int dailyWage = WAGE_PER_HRS * empWorkingHrs;
+        return dailyWage;
     }
 
     /**
@@ -29,6 +51,6 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        attendanceCheck();
+        System.out.println("Daily wage: " + dailyWage());
     }
 }
